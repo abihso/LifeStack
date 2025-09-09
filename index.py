f9023929e3be348ce87c420c17019d3d8b1b8ba3
abihso <solomonpwatio@gmail.com>
2025-09-09 02:25:12 +0000

    9/9/35

--- index.py
+++ index.py
@@ -1,27 +1,8 @@
-items = [
-  {
-  "id":1,
-  "name":"solomon pwatiu",
-  "grade":"C+",
-  "performance":"better"
-  },
-  {
-  "id":2,
-  "name":"kab pwatiu",
-  "grade":"B+",
-  "performance":"good"
-  },
-  {
-  "id":3,
-  "name":"kelly baba",
-  "grade":"C+",
-  "performance":"better"
-  },
-  {
-  "id":4,
-  "name":"sheila evans",
-  "grade":"A+",
-  "performance":"better"
-  },
-]
+from fastapi import FastAPI
 
+
+server = FastAPI()
+
+@server.get("/")
+async def index():
+  return ""
